11773d82da7ef6cc99f9a33f28fdf8d70fc2a1e0
DavidChn05 <david.chandra0501@gmail.com>
2023-12-03 14:41:39 +0700

    add landingPage & landingPageLogged

--- resources/views/landingPageLogged.blade.php
+++ resources/views/landingPageLogged.blade.php
@@ -6,7 +6,7 @@
         @vite('resources/css/app.css')
     </head>
     <body class="">
-        <nav class="flex bg-white shadow-xl h-fit justify-between px-20">
+        <nav class="flex bg-white shadow-xl h-fit justify-between items-center px-20">
             <div class="relative flex flex-row items-center p-4 font-bold text-3xl">
                 <h1>Satu</h1>
                 <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -27,16 +27,15 @@
                 <a href="" class="mx-8">
                     FAQ
                 </a>
-            </div>
-            <div class="relative flex items-center p-4">
-                <div class="relative flex items-center border-2 rounded-lg mx-2">
+                <div class="relative flex items-center border-2 rounded-lg mx-8">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="mx-2">
                         <path d="M21 21L16.657 16.657M16.657 16.657C17.3999 15.9141 17.9892 15.0322 18.3912 14.0615C18.7933 13.0909 19.0002 12.0506 19.0002 11C19.0002 9.94939 18.7933 8.90908 18.3912 7.93845C17.9892 6.96782 17.3999 6.08588 16.657 5.34299C15.9141 4.6001 15.0322 4.01081 14.0615 3.60877C13.0909 3.20672 12.0506 2.99979 11 2.99979C9.94939 2.99979 8.90908 3.20672 7.93845 3.60877C6.96782 4.01081 6.08588 4.6001 5.34299 5.34299C3.84266 6.84332 2.99979 8.87821 2.99979 11C2.99979 13.1218 3.84266 15.1567 5.34299 16.657C6.84332 18.1573 8.87821 19.0002 11 19.0002C13.1218 19.0002 15.1567 18.1573 16.657 16.657Z" stroke="#A6A6A6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
                         <input type="search" placeholder="Cari..." class="outline-none">
                 </div>
-                <button type="button" class="mx-2 px-4 font-bold text-sky-600 border-2 border-sky-600 rounded-lg">Masuk</button>
-                <button type="button" class="mx-2 px-4 font-bold bg-sky-600 border-2 border-sky-600 rounded-lg text-white">Daftar</button>
+            </div>
+            <div class="relative flex items-center p-4">
+                <p>Helo, User</p>
             </div>
         </nav>
         <div class="grid grid-cols-2 justify-center items-center py-20 bg-gradient-to-r from-sky-600 to-white">
